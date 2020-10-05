1e4e69e15a67acf2f94bc4ddef17ba00d3a3a7e4
skinksteek <linusigelstrom@gmail.com>
2020-10-05 15:14:02 +0200

    tredje sittning, hitta sida

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function openNavigation() {
     let nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+function initMap() {
+  var location = {lat: 59.322794, lng: 14.112214};
+  var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 17,
+      center: location
+  });
+  var marker = new google.maps.Marker({
+      position: location,
+      map: map
+  });
+}
